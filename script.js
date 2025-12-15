7789ca9be67ad3e1e8e6f1380ee8772f7281d73b
miiichelleo <michelle.otiji@fh-potsdam.de>
2025-12-15 16:49:18 +0100

    15.12

--- script.js
+++ script.js
@@ -1,35 +1,5 @@
-const overlay = document.getElementById('overlay');
-
-let revealed = false;
-let animating = false;
-
-window.addEventListener('wheel', (e) => {
-  if (animating) return;
-
-  // Scroll UP → remove design layer
-  if (e.deltaY < 0 && !revealed) {
-    animating = true;
-    revealed = false;
-    overlay.style.transform = 'translateY(-100%)';
-  }
-
-  // Scroll DOWN → bring design layer back
-  if (e.deltaY > 0 && !revealed) {
-    animating = true;
-    revealed = true;
-    overlay.style.transform = 'translateY(0)';
-  }
-
-  setTimeout(() => (animating = false), 900);
-});
 
 
-// AUTO REVEAL AFTER PAGE LOAD
-window.addEventListener('load', () => {
-  setTimeout(() => {
-    revealOverlay();
-  }, 600); // delay so circles are seen first
-});
 //Scroll-triggered Text animations
 
 const observerSettings = {
@@ -51,3 +21,4 @@ const observer = new IntersectionObserver((entries) => {
 document.querySelectorAll('.animate-element').forEach((element) => {
     observer.observe(element);
 });
+
